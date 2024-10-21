bf1c19a719b5a8891c10b2d669a6dcaaed7c61cb
ahmed <ahmed.student210@gmail.com>
2024-10-21 10:58:03 +0100

    change theme and create notes view

---
+++ lib/views/notees_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteesView extends StatelessWidget {
+  const NoteesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
